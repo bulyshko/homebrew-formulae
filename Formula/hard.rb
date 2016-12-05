5c12534a93e1248de2d7b8bccb714c0747c98731
Romuald Bulyshko <romuald@bulyshko.com>
2016-12-05 19:55:43 +0200

    sha1 → sha256

--- Formula/hard.rb
+++ Formula/hard.rb
@@ -3,7 +3,7 @@ require "formula"
 class Hard < Formula
   homepage "https://github.com/bulyshko/hard"
   url "https://github.com/bulyshko/hard/archive/1.0.0.tar.gz"
-  sha1 "a9d5324fb56faa834f9bcd157b7bf104d26d3f00"
+  sha256 "f3366f527baed6e333590d36c5ec3a35d3bae02fcd138a72539c7e5a973ce8fd"
 
   def install
     bin.mkdir
